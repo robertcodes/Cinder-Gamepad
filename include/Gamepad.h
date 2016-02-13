28691017d5da417d79e7192178d0a9296d4f976c
robertcodes <rob@iwillcodeforyou.com>
2016-02-12 20:07:12 -0500

    Updating to VS2013

--- include/Gamepad.h
+++ include/Gamepad.h
@@ -27,14 +27,14 @@ extern "C" {
 #endif
 
 #ifndef __cplusplus
-
+typedef int bool;
 #define	false	0
 #define	true	1
 
-#define	bool	_Bool
-#if __STDC_VERSION__ < 199901L && __GNUC__ < 3
-typedef	int	_Bool;
-#endif
+//#define	bool	_Bool
+//#if __STDC_VERSION__ < 199901L && __GNUC__ < 3
+//typedef	int	_Bool;
+//#endif
 
 #endif /* !__cplusplus */
 
